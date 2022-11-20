2faf8242fe3761985c10a8d58b904858e9d97057
LPT0317 <75077760+LPT0317@users.noreply.github.com>
2022-11-20 19:30:29 +0700

    Basic Project

---
+++ EM-System.X/Core/Src/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: lephu
+ *
+ * Created on November 20, 2022, 4:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (0);
+}
+
---
+++ EM-System.X/build/default/production/Core/Src/main.o.d
@@ -0,0 +1,3 @@
+build/default/production/Core/Src/main.o:  \
+	D:/PIC18/Project/Environmental-Monitoring-System/EM-System.X/Core/Src/main.c  \
+
